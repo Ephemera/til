993f537085b4377a1b3cd723f03b0df7fe86d318
GyuYong Jung <obliviscence@gmail.com>
2018-06-11 22:33:17 +0900

    htdp: chapter7

---
+++ htdp/chapter7.rkt
@@ -0,0 +1,42 @@
+#lang racket
+
+;; 7.2 혼합 데이터를 다루는 함수 디자인
+;; 7.2.1  동물원의 동물들
+;; 구조체:
+(define-struct spider (remaining-legs moving-area))
+(define-struct elephant (moving-area))
+(define-struct monkey (intelligence moving-area))
+
+;; 데이터 정의:
+;; 동물은 다음의 세 구조체 중 하나이다
+;; 1. (make-spider n a)
+;; n은 number, a는 number 
+;; 2. (make-elephant a)
+;; a는 number
+;; 3. (make-monkey n a)
+;; n은 number, a는 number 
+
+;; 템플릿:
+;; (define (f an-animal)
+;;   (cond
+;;     [(spider? an-animal)
+;;       ... (spider-remaining-legs an-animal) ... (spider-moving-area an-animal) ...]
+;;     [(elephant? an-animal)
+;;       ... (elephant-moving-area an-animal) ... ]
+;;     [(monkey? an-animal)
+;;       ... (monkey-intelligence an-animal) ... (monkey-moving-area an-animal) ... ]))
+
+;; 정의:
+(define (get-moving-area an-animal)
+  (cond
+    [(spider? an-animal) (spider-moving-area an-animal)]
+    [(elephant? an-animal) (elephant-moving-area an-animal)]
+    [(monkey? an-animal) (monkey-moving-area an-animal)]))
+
+(define (fits? an-animal cage-size)
+  (cond
+    [(>= cage-size (get-moving-area an-animal)) 'Enough]
+    [else 'NeedMore]))
+
+
+
